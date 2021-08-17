460dd84fd4de144fa41d429ab32a8922847fe587
renan <renan.manshio@gmail.com>
2021-08-17 03:15:06 -0300

    pass all tests

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
-
-var app = express();
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'})
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +12,12 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// The Upload 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => { 
+  const upFile = req.file;
+  res.json({name: upFile.originalname, type: upFile.mimetype, size: upFile.size})
+});
 
 
 const port = process.env.PORT || 3000;
